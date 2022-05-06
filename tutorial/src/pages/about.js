5b7372f3471cdf54d43148b7f28ceb5f05f654ab
ElfGod <albertogx@icloud.com>
2022-05-05 20:50:52 -0500

    CSS Modules

--- tutorial/src/pages/about.js
+++ tutorial/src/pages/about.js
@@ -1,11 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
+// import { page, text } from "../examples/about.module.css"
+import * as aboutStyles from "../examples/about.module.css"
 
 const about = () => {
   return (
     <Layout>
-      <div className="about-heading">
+      <div className={aboutStyles.page}>
         <h1>about page</h1>
+        <p className={aboutStyles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptas
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptas
+        </p>
       </div>
     </Layout>
   )
--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,14 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
+import { page, text } from "../examples/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <div className="home-heading">
+      <div className={page}>
         <h1>home page</h1>
         <h1>hello world</h1>
         <h1>hello people</h1>
+        <p className={text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptas
+          cupiditate dolores fuga nobis quam cumque ullam numquam magni dicta
+          voluptatibus, suscipit repellendus maxime nemo placeat! Amet ad
+          expedita nemo!{" "}
+        </p>
       </div>
     </Layout>
   )
