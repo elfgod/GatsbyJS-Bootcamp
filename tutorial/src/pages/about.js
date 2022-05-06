6ae45838ab6f04e57fc1ea9771a9cf1994cd1c56
ElfGod <albertogx@icloud.com>
2022-05-05 21:34:53 -0500

    Styled-Components

--- tutorial/src/pages/about.js
+++ tutorial/src/pages/about.js
@@ -2,19 +2,32 @@ import React from "react"
 import Layout from "../components/Layout"
 // import { page, text } from "../examples/about.module.css"
 import * as aboutStyles from "../examples/about.module.css"
+import styled from "styled-components"
 
 const about = () => {
   return (
     <Layout>
-      <div className={aboutStyles.page}>
+      <Wrapper>
         <h1>about page</h1>
-        <p className={aboutStyles.text}>
+        <p className="text">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptas
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad voluptas
         </p>
-      </div>
+      </Wrapper>
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: brown;
+
+  h1 {
+    color: yellowgreen;
+  }
+
+  .text {
+    text-transform: uppercase;
+  }
+`
 
 export default about
