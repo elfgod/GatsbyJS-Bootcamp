bdb9d29bd98f63774e6307b107a02e8353c1f9a1
ElfGod <albertogx@icloud.com>
2022-05-08 23:12:39 -0500

    Sandbox Setup

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Images from "../examples/Images"
 
 const Home = () => {
   return (
     <Layout>
       <h1>home page</h1>
+      <Images />
     </Layout>
   )
 }
