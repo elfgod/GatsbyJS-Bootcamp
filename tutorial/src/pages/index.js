f93295177167a4eb38fcc7c7c43f43c341996f3a
ElfGod <albertogx@icloud.com>
2022-05-10 22:46:10 -0500

    About Page

--- tutorial/src/pages/index.js
+++ tutorial/src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 const Home = () => {
   return (
     <Layout>
       <h1>home page</h1>
-      <Images />
     </Layout>
   )
 }
