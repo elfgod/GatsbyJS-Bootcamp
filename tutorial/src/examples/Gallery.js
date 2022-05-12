1accac6cfd81037f3220456757f205e3b70a4841
ElfGod <albertogx@icloud.com>
2022-05-11 21:55:47 -0500

    Gallery Setup

---
+++ tutorial/src/examples/Gallery.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Gallery = () => {
+  return (
+    <div>
+      <h1>Simple Image Gallery</h1>
+    </div>
+  )
+}
+
+export default Gallery
--- tutorial/src/pages/testing.js
+++ tutorial/src/pages/testing.js
@@ -1,35 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Gallery from "../examples/Gallery"
 
-const Testing = ({ data }) => {
-  console.log(data)
-  const author = data.site.info.author
+const Testing = () => {
   return (
-    <div>
-      <h2>Testing</h2>
-      <h3>Author: {author}</h3>
-    </div>
+    <Layout>
+      <main className="page">
+        <Gallery />
+      </main>
+    </Layout>
   )
 }
-
-export const data = graphql`
-  {
-    site {
-      info: siteMetadata {
-        person {
-          age
-          name
-        }
-        complexData {
-          age
-          name
-        }
-        author
-        description
-        simpleData
-      }
-    }
-  }
-`
 
 export default Testing
