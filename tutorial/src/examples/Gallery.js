79b986afcd78df6b1930fcf1acade1e543a0b3de
ElfGod <albertogx@icloud.com>
2022-05-25 09:00:18 -0500

    gatsbyImageData - Field

--- tutorial/src/examples/Gallery.js
+++ tutorial/src/examples/Gallery.js
@@ -1,6 +1,28 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+const query = graphql`
+  {
+    allFile(filter: { extension: { ne: "svg" } }) {
+      nodes {
+        name
+        childImageSharp {
+          id
+          gatsbyImageData(
+            layout: FIXED
+            placeholder: BLURRED
+            transformOptions: { grayscale: true }
+          )
+        }
+      }
+    }
+  }
+`
 
 const Gallery = () => {
+  const data = useStaticQuery(query)
+  console.log(data)
   return (
     <div>
       <h1>Simple Image Gallery</h1>
